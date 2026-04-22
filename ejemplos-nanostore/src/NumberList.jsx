f96cfd850075df88041b3a6fd7d4911db0d68120
Pau Fernández <pau.fernandez@upc.edu>
2026-04-22 18:37:11 +0200

    Puliendo cosas pequeñas

--- ejemplos-nanostore/src/NumberList.jsx
+++ ejemplos-nanostore/src/NumberList.jsx
@@ -6,8 +6,11 @@ export default function NumberList() {
 
     return (
         <div className="border grid grid-cols-10 p-4 gap-4">
-            {numberList.map((n) => (
-                <div className="aspect-square border border-gray-300 flex flex-row justify-center items-center rounded-full text-2xl">
+            {numberList.map((n, i) => (
+                <div
+                    key={i}
+                    className="aspect-square border border-gray-300 flex flex-row justify-center items-center rounded-full text-2xl"
+                >
                     {n}
                 </div>
             ))}
